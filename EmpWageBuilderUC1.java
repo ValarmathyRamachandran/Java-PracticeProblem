4c74fede4e8e9ac2d39cb2f33bb360fc1b2d412a
Valarmathy <valarmathy.ramachandran@outlook.com>
2021-09-30 00:04:18 +0530

    Checked whether Employee is Present or Absent

--- EmpWageBuilderUC1.java
+++ EmpWageBuilderUC1.java
@@ -2,6 +2,16 @@ public class EmpWageBuilderUC1 {
 
 	public static void main(String[] args) {
 
-		System.out.println("Welcome to Employee Wage Computation");
-	}
+		System.out.println( "Welcome to Employee Wage Computation Program" );
+
+		int isFullTime= 1;
+			//computations
+		double empCheck = Math.floor(Math.random() * 10) % 2;
+			// checking the condition
+		if (empCheck == isFullTime )
+			System.out.println( "Employee is Present" );
+		else
+			System.out.println( "Employee is Absent" );
+
+			}
 }
