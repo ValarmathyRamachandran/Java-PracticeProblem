050ee6447cd0caad8ca8310415a5764dbca0fdca
ValarmathyRamachandran <90239548+ValarmathyRamachandran@users.noreply.github.com>
2021-09-30 19:38:31 +0530

    Create SnakeAndLadderUC1.java
    New Folder added

---
+++ SnakeAndLadder-UC/SnakeAndLadderUC1.java
@@ -0,0 +1,6 @@
+public class SnakeAndLadderUC1 {
+	public static void main(String[] args) {
+		int numOnDice= Math.ceil(Math.random() * 10) % 6 +1);
+		System.out.println( Player rolled dice at start postion 0 and got number: " +numOnDice);
+	}
+}
