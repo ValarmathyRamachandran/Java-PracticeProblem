e86bff3b8bd0bb661ae7654ba03b92ec6b648ef8
Valarmathy <valarmathy.ramachandran@outlook.com>
2021-09-30 13:13:54 +0530

    Compared Whether two lines are equal or not

---
+++ ComparisonUC2.java
@@ -0,0 +1,25 @@
+public class ComparisonUC1 {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Line Comparison Computation Program");
+
+
+ 		int x1,x2,y1,y2;
+	    double len;
+	    x1=1;y1=1;x2=4;y2=4;
+	    len=Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
+            System.out.println("Length (x1,y1) (x2,y2) :"+"(" +x1+","+y1+"),"+"("+x2+","+y2+")===>"+len);
+
+		if(x1==y1 && x2==y2)
+		{
+			System.out.println("Two Lines are equal");
+		}
+		else
+		{
+			System.out.println("Two Lines are unequal");
+		}
+	}
+}
+
+
+
