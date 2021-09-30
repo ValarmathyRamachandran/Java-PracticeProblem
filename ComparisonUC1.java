be69456feebeac5d21f21f9241311269795935d1
Valarmathy <valarmathy.ramachandran@outlook.com>
2021-09-30 12:57:26 +0530

    Calculated the length of line using Cartesian system

---
+++ ComparisonUC1.java
@@ -0,0 +1,16 @@
+public class ComparisonUC1 {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Line Comparison Computation Program");
+
+
+ 		int x1,x2,y1,y2;
+	    double len;
+	    x1=1;y1=1;x2=4;y2=4;
+	    len=Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
+            System.out.println("Length:"+"("+x1+","+y1+"),"+"("+x2+","+y2+")===>"+len);
+	}
+}
+
+
+
