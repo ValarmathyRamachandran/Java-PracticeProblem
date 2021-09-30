f89fe3cd92339b1bb7a33065799e0d11f787b5f6
Valarmathy <valarmathy.ramachandran@outlook.com>
2021-09-30 18:51:35 +0530

    Updated the program using while loop for calc wages

--- EmpWageBuilderUC6.java
+++ EmpWageBuilderUC6.java
@@ -1,49 +1,34 @@
 public class EmpWageBuilderUC6 {
+	public static final int IS_FULL_TIME = 1;
+    	public static final int IS_PART_TIME = 2;
+	public static final int EMP_RATE_PER_HOUR = 20;
+    	public static final int NUM_OF_WORKING_DAYS = 20;
+	public static final int MAX_HRS_IN_MONTH = 100;
 
-	public static void main(String[] args) {
+        public static void main(String[] args) {
 
-		System.out.println( "Welcome to Employee Wage Computation Program" );
+                System.out.println( "Welcome to Employee Wage Computation Program");
 
-		int isFullTime= 1;
-		int isPartTime= 2;
-		int fullDayHour= 0;
-		int partTimeHour= 0;
-		int empWagePerHour=20;
-		int empWorkingDayPerMonth=20;
-			//computations
-		int empCheck = (int)Math.round((Math.random() *10) % 3);
-			// checking the condition
-		switch (empCheck)
-		{
-			case 1:
-				for(int i=1;i<=20;i++)
-				{
-					System.out.println ( "Day no: " +i);
-					fullDayHour=8;
-			int fullTimeEmpDailyWage=(empWagePerHour*fullDayHour);
-			int fullTimeEmpMonthlyWage=(empWagePerHour*fullDayHour*i);
+		int empHrs =0, totalEmpHrs=0, totalWorkingDays =0;
 
-				System.out.println( "Employee is Present as Full-Time" );
-				System.out.println( "Full-Time Employee Daily Wage :" +fullTimeEmpDailyWage);
-				System.out.println( "Full-Time Employee Monthly Wage: " +fullTimeEmpMonthlyWage);
-
+                while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
+                totalWorkingDays++;
+                int empCheck = (int)Math.round((Math.random() *10) % 3);
+                switch (empCheck)
+                {
+						case IS_FULL_TIME:
+								empHrs=8;
+								break;
+						case IS_PART_TIME:
+								empHrs=4;
+								break;
+						default:
+								empHrs=0;
 				}
-			break;
-			case 2:
-
-					partTimeHour=8;
-			int partTimeDailyEmpWage=(empWagePerHour*partTimeHour);
-			int partTimeMonthlyEmpWage=(empWagePerHour*partTimeHour*empWorkingDayPerMonth);
-				System.out.println( "Employee is Present as Part-Time");
-				System.out.println( "Part-Time Daily Employee Wage: " +partTimeDailyEmpWage);
-				System.out.println( "Part-Time Monthly Wage: " +partTimeMonthlyEmpWage);
-			break;
-			default:
-			System.out.println( "Employee is Absent" );
-
+				totalEmpHrs += empHrs;
+				System.out.println("day:" + totalWorkingDays +" " + "Emp hr: " +empHrs);
+			}
+			int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
+			System.out.println("Total Emp Wage: " + totalEmpWage);
 		}
-
-		}
-
-	}
-
+}
